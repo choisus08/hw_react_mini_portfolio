76fe2674f740eccaddfa8869d95227fabd79fcfd
Susie Gordon <susieyoungchoi@gmail.com>
2023-07-19 08:24:59 -0400

    import projects.js to main.js

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,9 @@
+import Projects from "./Projects";
+
 function Main(props) {
-    return <div className="Main">This is the Main</div>
+    return <div className="Main">
+        <Projects />
+    </div>
 }
 
 export default Main;
---
+++ src/components/Projects.js
@@ -0,0 +1,5 @@
+function Projects(props) {
+    return <div className="Projects">This is Projects</div>
+}
+
+export default Projects;
