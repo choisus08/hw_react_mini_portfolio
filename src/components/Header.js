be75da2c56628b828470cb36ac9743d70d3d650b
Susie Gordon <susieyoungchoi@gmail.com>
2023-07-19 08:20:46 -0400

    create header as navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,12 @@
 function Header(props) {
-    return <div className="Header">This is the Header</div>
+    return (
+    <div className="Header">
+        <ul className="Navbar">
+            <li><a href="https://github.com/choisus08?tab=repositories">Github</a></li>
+            <li><a href="https://www.linkedin.com/in/gordon-susie/">LinkedIn</a></li>
+        </ul>
+    </div>
+    )
 }
 
 export default Header;
